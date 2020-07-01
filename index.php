5e539eb9d5b7d439963e7c40fa5b5f327588410d
EliuPuente <44873801+Eliupuente@users.noreply.github.com>
2020-07-01 12:34:59 -0500

    asd
    asd

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
-<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<link href="http://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <!------ Estilos ---------->
 <!DOCTYPE html>
